f5b7db220af1ffe33627e915d40f7d6db68317c5
Colette <hello@colettesmith.com>
2023-06-22 11:56:03 +0100

    provider

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import mySlice from './features/mySlice'
-
+import mySlice from '../features/mySlice'
 export const store = configureStore({
     reducer : {
         mySlice : mySlice
