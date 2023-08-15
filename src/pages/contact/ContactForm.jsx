5b8d252d41753d2fa2a01194490f89d5a010f3f4
Colette <hello@colettesmith.com>
2023-08-15 09:06:05 +0100

    hooked up with Web3Forms

--- src/pages/contact/ContactForm.jsx
+++ src/pages/contact/ContactForm.jsx
@@ -1,3 +1,64 @@
+// import React, { useState } from "react";
+// import { validate } from "../../validation";
+// import { useDispatch } from "react-redux";
+// import { setToastContent } from "../../features/appSlice";
+
+// const ContactForm = () => {
+//   const dispatch = useDispatch();
+//   // const contactForm = useSelector(selectContactForm);
+
+//   const [errors, setErrors] = useState({});
+
+//   const [userInput, setUserInput] = useState({});
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     const res = await validate(userInput, "contactForm");
+
+//     if (!res) {
+//       //do something with form - when we got backend
+//       dispatch(setToastContent("Thanks for getting in touch!"));
+//       return;
+//     }
+//     setErrors(res);
+//     // console.log(res);
+//   };
+
+//   return (
+//     <form
+//       onInput={(e) => {
+//         setUserInput({ ...userInput, [e.target.name]: e.target.value });
+//       }}
+//       onSubmit={handleSubmit}
+//     >
+//       <div>
+//         <label htmlFor="name">Name:</label>
+//         <input type="text" id="name" name="name" required />
+//         <p>{errors.name}</p>
+//       </div>
+//       <div>
+//         <label htmlFor="email">Email:</label>
+//         <input type="email" id="email" name="email" required />
+//         <p>{errors.email}</p>
+//       </div>
+//       <div>
+//         <label htmlFor="message">Message:</label>
+//         <textarea id="message" name="message" required />
+//         <p>{errors.message}</p>
+//       </div>
+//       <button type="submit">submit</button>
+//       <div className="contactMsg">
+//         <p>
+//           Jimmy will get back to you asap! <br></br>Thanks for getting in touch.
+//         </p>
+//       </div>
+//     </form>
+//   );
+// };
+
+// export default ContactForm;
+
 import React, { useState } from "react";
 import { validate } from "../../validation";
 import { useDispatch } from "react-redux";
@@ -5,24 +66,30 @@ import { setToastContent } from "../../features/appSlice";
 
 const ContactForm = () => {
   const dispatch = useDispatch();
-  // const contactForm = useSelector(selectContactForm);
-
   const [errors, setErrors] = useState({});
-
   const [userInput, setUserInput] = useState({});
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    const formData = new FormData(e.target);
+    formData.append("access_key", "babf4c44-14e9-42b5-82e4-1ac8e74336e1");
     const res = await validate(userInput, "contactForm");
 
     if (!res) {
-      //do something with form - when we got backend
-      dispatch(setToastContent("hello thanks spspdowj"));
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData,
+      }).then((res) => res.json());
+
+      if (response.success) {
+        dispatch(setToastContent("Thanks for getting in touch!"));
+      } else {
+        console.error("Error from API:", response.message);
+      }
       return;
     }
     setErrors(res);
-    // console.log(res);
   };
 
   return (
@@ -47,12 +114,14 @@ const ContactForm = () => {
         <textarea id="message" name="message" required />
         <p>{errors.message}</p>
       </div>
-      <button type="submit">submit</button>
+      <button type="submit">Submit</button>
+      {/* Dont need this message cos of toastify ....
+      
       <div className="contactMsg">
         <p>
           Jimmy will get back to you asap! <br></br>Thanks for getting in touch.
         </p>
-      </div>
+      </div> */}
     </form>
   );
 };
