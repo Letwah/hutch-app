7c0fa12e11438bfb722a414ef43c68f1eadc97a7
Colette <hello@colettesmith.com>
2024-03-06 15:56:55 +0000

    tweaks

--- src/pages/FineArt.jsx
+++ src/pages/FineArt.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Search from "./fineArt/Search";
 import Sort from "./fineArt/Sort";
