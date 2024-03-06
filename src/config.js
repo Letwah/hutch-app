d8f6f9560ada588b25c872f5e7013b931a2d0ee6
Colette <hello@colettesmith.com>
2024-03-06 14:43:15 +0000

    change of link for copyright

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 export const urlArtInstagram = "https://www.instagram.com/byhutchart/";
 export const urlIllustrationInsta = "https://www.linkedin.com/in/jimmy-hutch/";
-export const urlColetteSmithDeveloper =
-  "https://www.linkedin.com/in/colette-smith-developer/";
+export const urlColetteSmithDeveloper = "https://www.colettesmith.uk";
 
 // export const apiURL = "http://localhost:6003";
 export const apiURL = "https://api.jimmyhutch.uk";
